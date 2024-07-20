9ab9ddfc2cdb4402f3d21d19a9e399ab5a39ce2d
Ishan-Chanuka <ishanchanuka4@cmail.com>
2024-07-20 11:02:58 +0530

    Add project files.

---
+++ src/Services/Papyrus.Docs.DocumentApi/Models/DbModels/MeetingMinutes.cs
@@ -0,0 +1,12 @@
+﻿using Papyrus.Docs.DocumentApi.Primitives.BaseEntity;
+
+namespace Papyrus.Docs.DocumentApi.Models.DbModels
+{
+    public class MeetingMinutes : BaseEntity
+    {
+        public required string Title { get; set; }
+        public required string MeetingNo { get; set; }
+        public string? Description { get; set; }
+        public DateTime MeetingDate { get; set; }
+    }
+}
---
+++ src/Services/Papyrus.Docs.DocumentApi/Primitives/BaseEntity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Papyrus.Docs.DocumentApi.Primitives.BaseEntity
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public int CreatedUser { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int? ModifiedUser { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
